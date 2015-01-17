4df6d98e3ff53de2105cae693434e36c302bb95b
Christian Forster <ch.forster@gmail.com>
2015-01-17 17:12:02 +0100

    added random direction sampling

--- vikit_common/include/vikit/sample.h
+++ vikit_common/include/vikit/sample.h
@@ -3,6 +3,7 @@
 
 #include <random>
 #include <chrono>
+#include <Eigen/Core>
 
 namespace vk {
 
@@ -15,6 +16,8 @@ public:
   static double gaussian(double sigma);
   static std::ranlux24 gen_real;
   static std::mt19937 gen_int;
+  static Eigen::Vector3d randomDirection3D();
+  static Eigen::Vector2d randomDirection2D();
 };
 
 std::ranlux24 Sample::gen_real;
@@ -43,6 +46,24 @@ double Sample::gaussian(double stddev)
 {
   std::normal_distribution<double> distribution(0.0, stddev);
   return distribution(gen_real);
+}
+
+Eigen::Vector3d Sample::randomDirection3D()
+{
+  // equal-area projection according to:
+  // https://math.stackexchange.com/questions/44689/how-to-find-a-random-axis-or-unit-vector-in-3d
+  const double z = Sample::uniform()*2.0-1.0;
+  const double t = Sample::uniform()*2.0*M_PI;
+  const double r = std::sqrt(1.0 - z*z);
+  const double x = r*std::cos(t);
+  const double y = r*std::sin(t);
+  return Eigen::Vector3d(x,y,z);
+}
+
+Eigen::Vector2d Sample::randomDirection2D()
+{
+  const double theta = Sample::uniform()*2.0*M_PI;
+  return Eigen::Vector2d(std::cos(theta), std::sin(theta));
 }
 
 } // namespace vk
