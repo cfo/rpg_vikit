bb319d69565d68238b1497284aedc2f1e207e9e9
Christian Forster <ch.forster@gmail.com>
2015-03-14 13:39:20 +0100

    vikit_common and vikit_ros use minkindr instead of sophus.

--- vikit_ros/src/output_helper.cpp
+++ vikit_ros/src/output_helper.cpp
@@ -11,29 +11,31 @@
 namespace vk {
 namespace output_helper {
 
-void
-publishTfTransform(const Sophus::SE3& T, const ros::Time& stamp,
-                   const string& frame_id, const string& child_frame_id,
-                   tf::TransformBroadcaster& br)
+void publishTfTransform(
+    const Transformation& T,
+    const ros::Time& stamp,
+    const string& frame_id,
+    const string& child_frame_id,
+    tf::TransformBroadcaster& br)
 {
   tf::Transform transform_msg;
-  Eigen::Quaterniond q(T.rotation_matrix());
-  transform_msg.setOrigin(tf::Vector3(T.translation().x(), T.translation().y(), T.translation().z()));
+  const Eigen::Quaterniond& q = T.getRotation().toImplementation();
+  transform_msg.setOrigin(tf::Vector3(T.getPosition().x(), T.getPosition().y(), T.getPosition().z()));
   tf::Quaternion tf_q; tf_q.setX(q.x()); tf_q.setY(q.y()); tf_q.setZ(q.z()); tf_q.setW(q.w());
   transform_msg.setRotation(tf_q);
   br.sendTransform(tf::StampedTransform(transform_msg, stamp, frame_id, child_frame_id));
 }
 
-void
-publishPointMarker(ros::Publisher pub,
-                   const Vector3d& pos,
-                   const string& ns,
-                   const ros::Time& timestamp,
-                   int id,
-                   int action,
-                   double marker_scale,
-                   const Vector3d& color,
-                   ros::Duration lifetime)
+void publishPointMarker(
+    ros::Publisher pub,
+    const Vector3d& pos,
+    const string& ns,
+    const ros::Time& timestamp,
+    int id,
+    int action,
+    double marker_scale,
+    const Vector3d& color,
+    ros::Duration lifetime)
 {
   visualization_msgs::Marker msg;
   msg.header.frame_id = "/world";
